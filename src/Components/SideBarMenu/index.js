714ff87950ea936d4b78a75db997d5534e0101cc
Gabriel <gabrielprogramado@gmail.com>
2024-03-07 12:01:07 -0300

    feat(SideBar): Add sidebar menu to SideBarMenu component

--- src/Components/SideBarMenu/index.js
+++ src/Components/SideBarMenu/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Nav} from "react-bootstrap";
+import Logo from '../../assets/images/logo.svg';
 
-// import { Container } from './styles';
+import '../../assets/css/sidebar.css';
 
 function SideBarMenu() {
-  return <div />;
+  return (
+    <Nav 
+        className="col-md-12 d-none d-md-block bg-white sidebar"
+        fill
+    >
+        <div className="sidebar-sticky">
+            <img src={Logo} alt="Logo" className="logo" />
+        </div>
+        <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+    </Nav>
+  );
 }
 
 export default SideBarMenu;
