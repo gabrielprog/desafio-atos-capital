8711d0b66738b0b5342b74a3d580e1d2a4d8014c
Gabriel <gabrielprogramado@gmail.com>
2024-03-07 12:02:12 -0300

    feat(Dashboard page): Add sidebar menu to Dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import SideBarMenu from '../../Components/SideBarMenu';
 
 function Dashboard() {
   return (
-    <h1>Dashboard</h1>
+    <>
+      <Container
+        style={{padding: 0, margin: 0, height: "100%"}}
+      >
+          {/* TODO: conserta o width da sidebar */}
+            <Row>
+                <Col xs={2} id="sidebar-wrapper" style={{ maxWidth: '200px' }}>   
+                  <SideBarMenu />
+                </Col>
+                <Col xs={10} id="page-content-wrapper">
+                    this is a test
+                </Col> 
+            </Row>
+
+        </Container>
+    </>
   );
 }
 
